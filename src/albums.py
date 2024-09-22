b7f3cb17c6b0be8f1c9a5acfd80fd5c1f8d2c983
Martin Felipe Espinal Cruces <cofy43@ciencias.unam.mx>
2024-09-22 16:14:14 -0600

    dosn´t exist delete album function

--- src/albums.py
+++ src/albums.py
@@ -28,10 +28,3 @@ def get_photos_in_album(service: build, album_id: str) -> list:
             break
 
     return photos
-
-def delete_album(service, album_id):
-    try:
-        service.albums().delete(albumId=album_id).execute()
-        print(f"Album {album_id} deleted successfully.")
-    except Exception as e:
-        print(f"Error deleting album: {e}")
